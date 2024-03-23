3a67375139f5c9e3777326d5041004dbe8990016
Kim1850 <kim.st-pierre.2@ulaval.ca>
2024-03-23 14:55:24 -0400

    data migration done

---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,21 @@
+ALTER TABLE singer RENAME TO musician;
+ALTER TABLE musician RENAME COLUMN singerName TO musicianName;
+ALTER TABLE musician ADD COLUMN role VARCHAR(50);
+ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
+UPDATE musician
+SET role = 'vocals', bandName = 'Crazy Duo'
+WHERE musicianName = 'Alina' AND firstName = 'Darcy' AND lastName = 'Boles';
+UPDATE musician
+SET role = 'guitar', bandName = 'Mysterio'
+WHERE musicianName = 'Mysterio' AND firstName = 'Jessie' AND lastName = 'Chancey';
+UPDATE musician
+SET role = 'percussion', bandName = 'Crazy Duo'
+WHERE musicianName = 'Rainbow' AND firstName = 'Sarah' AND lastName = 'Derrick';
+UPDATE musician
+SET role = 'piano', bandName = 'Luna'
+WHERE musicianName = 'Luna' AND firstName = 'Emily' AND lastName = 'Seibold';
+
+CREATE TABLE band (bandName VARCHAR(50) PRIMARY KEY, creation YEAR, genre VARCHAR(50));
+INSERT INTO band VALUES ('Crazy Duo', 2015, 'rock');
+INSERT INTO band VALUES ('Luna', 2009, 'classical');
+INSERT INTO band VALUES ('Mysterio', 2019, 'pop');
